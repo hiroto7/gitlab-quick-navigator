bd3d3e06b612a44bc3e1b699b330c67394979b2d
hiroto7 <hiroto.dev@gmail.com>
2024-03-23 23:44:12 +0900

    設定済みのアクセストークンを一覧表示したり削除したりできるようにする
    Closes #5

---
+++ src/options/App.tsx
@@ -0,0 +1,45 @@
+import { Button, Listbox, ListboxItem } from "@nextui-org/react";
+import { useChromeStorage } from "../hooks";
+
+const App: React.FC = () => {
+  const tokens = useChromeStorage("local") as
+    | Record<string, string>
+    | undefined;
+
+  if (tokens === undefined) return undefined;
+
+  return (
+    <Listbox
+      shouldHighlightOnFocus={false}
+      className="max-w-xl mx-auto"
+      topContent="設定済みのアクセストークン"
+    >
+      {Object.keys(tokens).map((origin) => (
+        <ListboxItem
+          showDivider
+          key={origin}
+          shouldHighlightOnFocus={false}
+          endContent={
+            <Button
+              size="sm"
+              onPress={() => {
+                if (
+                  confirm(
+                    `${origin}用のアクセストークンを削除してもよろしいですか？`
+                  )
+                )
+                  chrome.storage.local.remove(origin);
+              }}
+            >
+              削除
+            </Button>
+          }
+        >
+          {origin}
+        </ListboxItem>
+      ))}
+    </Listbox>
+  );
+};
+
+export default App;
