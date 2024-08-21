88cf7201714574ba57ea9fab06cc90ee6ca1284c
hiroto7 <hiroto.dev@gmail.com>
2024-08-21 23:46:59 +0900

    Install eslint-plugin-react-compiler

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,12 +11,13 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "react-compiler"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "react-compiler/react-compiler": "error",
   },
   parserOptions: {
     ecmaVersion: "latest",
