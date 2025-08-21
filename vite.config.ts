26ed688889b61cb017750f6625112b20ed83d19a
hiroto7 <hiroto.dev@gmail.com>
2025-08-22 00:07:43 +0900

    Development Serverで開発できるよう修正

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest }), tailwindcss()],
+  server: { cors: { origin: [/chrome-extension:\/\//] } },
 });
